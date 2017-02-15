e8a5719f56b77dca9702fcb3b50dfb887aaf2c12
tbrams <torben@brams.dk>
2017-02-15 20:16:39 +0700

    Working prototype

--- app/src/main/java/dk/brams/android/openair1/MapsActivity.java
+++ app/src/main/java/dk/brams/android/openair1/MapsActivity.java
@@ -3,6 +3,7 @@ package dk.brams.android.openair1;
 import android.graphics.Color;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -10,6 +11,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polygon;
 import com.google.android.gms.maps.model.PolygonOptions;
@@ -21,12 +23,14 @@ import java.util.regex.Pattern;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
     public static final String TAG = "TBR:";
-    public static final int STEP_SIZE = 5;
+    public static final int STEP_SIZE = 1;
 
     private GoogleMap mMap;
-    private LatLng mCenter=null;
-    private int mStep_direction=1;
+    private LatLng mCenter = null;
+    private int mStep_direction = 1;
     private ArrayList<LatLng> mCoordList = new ArrayList<>();
+    private int mOutlineColor;
+    private Marker mAirportMarker = null;
 
 
     @Override
@@ -36,6 +40,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
+
+
         mapFragment.getMapAsync(this);
     }
 
@@ -61,52 +67,96 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng KRNO = parseCoordinateString("39:29.9 N 119:46.1W");
 
 
-        mMap.addMarker(new MarkerOptions().position(KRNO).title("KRNO Airport"));
+        mAirportMarker = mMap.addMarker(new MarkerOptions().position(KRNO).title("KRNO Airport"));
+
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(KRNO, 8));
 
-        /*
         String[] openAirCommands = {
-            "V X=39:29.9 N 119:46.1W",
-            "DA 10,270,290",
-            "DA 7,290,320",
-            "DA 10,320,200",
-            "V D=-",
-            "DA 5,200,270"
-            };
-*/
-
-        String[] openAirCommands = {
+                /*
+                "* Reno CTR",
+                "AC C",
+                "AN RENO-C",
+                "AL SFC",
+                "AH 8400 ft",
+                "V X=39:29.9 N 119:46.1 W",
+                "DC 5",
+                "",
+                "* Reno TMA xx",
+                "AC C",
                 "V X=39:29.9 N 119:46.1W",
                 "DA 10,270,290",
                 "DA 7,290,320",
                 "DA 10,320,200",
                 "V D=-",
-                "DA 5,200,270"
+                "DA 5,200,270",
+                "",
+
+                */
+                "AC C",
+                "AN BEALE AFB",
+                "AL SFC",
+                "AH 4100 ft",
+                "V X=39:08.2 N 121:26.2 W",
+                "DC 5",
+                "",
+                "AC C",
+                "AN BEALE AFB",
+                "AL 2600ft",
+                "AH 4100 ft",
+                "AT 39:10.2 N 121:17.2 W",
+                "DA 10,9,130",
+                "V D=-",
+                "DA 5,130,9",
+                "",
+                "AC C",
+                "AN BEALE AFB",
+                "AL 1600ft",
+                "AH 4100 ft",
+                "AT 39:06.2 N 121:35.5 W",
+                "DA 10,130,9",
+                "V D=-5",
+                "DA 5,9,130"
         };
 
-        ArrayList<LatLng> coords = new ArrayList<>();
+        for (String cmd : openAirCommands) {
+            if (cmd.equals("")) {
 
-        for (String cmd: openAirCommands) {
-            parseCommand(cmd, coords);
+                plotAndReset();
+            } else {
+                parseCommand(cmd);
+            }
         }
+        plotAndReset();
 
+
+    }
+
+    private void plotAndReset() {
+
+        // Remove marker from display - otherwise it will become cluttered
+        mAirportMarker.setVisible(false);
+
+        // Create a Google Maps Polygon and populate it with coordinates interpreted form the OpenAir spec.
         PolygonOptions polyOptions = new PolygonOptions();
-        for (LatLng pos: mCoordList) {
+        for (LatLng pos : mCoordList) {
             polyOptions.add(pos);
         }
 
-        polyOptions
-                .strokeColor(Color.RED)
-                .fillColor(Color.BLUE);
+        // Use outline only to keep cluttering at a minimum, color of outline is defined by Airspace class
+        polyOptions.strokeColor(ContextCompat.getColor(getApplicationContext(), mOutlineColor));
+        //  .fillColor(Color.BLUE);
 
-        // Get back the mutable Polygon
+        // Display polygon
         Polygon polygon = mMap.addPolygon(polyOptions);
 
+        // Reset internal storage
+        mCoordList = new ArrayList<>();
+        mOutlineColor = R.color.Yellow;
     }
 
 
     /**
-     * Parse coordinates in the format used in the OpenAir language and return a LatLng object.
+     * Parse coordinates in the String format from OpenAir specification and return a LatLng object.
      *
      * Uses a Regular Expression to parse the components of a coordinate string, convert into double
      * and create a LatLng object that can be used in Google Maps.
@@ -121,6 +171,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         Pattern r = Pattern.compile(pattern);
         Matcher m = r.matcher(coordString);
         if (m.find()) {
+
             // Given a string like "39:29.9 N 119:46.1W" we will get 8 matches:
             // "39", "29", "9", "N" and "119", "46", "1", "W" starting at index 1
 
@@ -134,62 +185,128 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             return new LatLng(lat, lon);
 
         } else {
-            Log.e(TAG, "parseCoordinateString: Cannot parse coordinate: " + coordString);
+            Log.e(TAG, "parseCoordinateString: Cannot parse coordinate String: " + coordString);
             return null;
         }
     }
 
 
-    public void parseCommand(String cmd, ArrayList<LatLng> coordList) {
+    public void parseCommand(String cmd) {
 
-        // First pattern matches two groups - first is the command and the second is simply the rest of the line
-        String pattern = "(\\w+) ([\\w\\d\\s\\:\\.\\=\\+\\-\\,]*)";
+        // First pattern matches two groups - the main command and the rest of the line
+        String pattern = "(AN|AC|AL|AH|DC|DA|DP|V|\\*) ([\\w\\d\\s\\:\\.\\=\\+\\-\\,]*)";
         Pattern r = Pattern.compile(pattern);
         Matcher m = r.matcher(cmd);
 
         if (m.find()) {
-            // At this point, we should have the command and the arguments.
             String command = m.group(1).toUpperCase();
             String rest = m.group(2).trim().toUpperCase();
 
-            Log.d(TAG, String.format("%s -> %s, %s", cmd, command, rest));
+            LatLng pos = null;
+            int radius;
+            int fromDeg;
+            int toDeg;
 
-            // Further processing of arguments depending on command
             switch (command) {
+                case "*":
+                    // Comment - do nothing
+                    Log.d(TAG, "parseCommand: Comment");
+                    break;
+
+                case "AC":
+                    // Airspace Class - expect a simple one letter argument
+
+                    Log.d(TAG, "parseCommand: Airspace Class");
+
+                    mOutlineColor = R.color.Yellow;
+                    if (rest.equals("B")) {
+                        mOutlineColor = R.color.DodgerBlue;
+                    } else if (rest.equals("C")) {
+                        mOutlineColor = R.color.MediumVioletRed;
+                    } else if (rest.equals("D")) {
+                        mOutlineColor = R.color.Crimson;
+                    } else {
+                        Log.e(TAG, "Airspace argunt problem: " + rest);
+                    }
+                    break;
+
+
+                case "AN":
+                    // Airport name, expect string parameter
+                    // not implem,ented yet
+
+                    Log.d(TAG, "parseCommand: Airport Name");
+                    break;
+
+                case "AL":
+                    // Altitude Low, expect a parameters like "3500 ft" or "SFC"
+                    // Not implemented yet
+
+                    Log.d(TAG, "parseCommand: Altitude Low");
+                    break;
+
+
+                case "AH":
+                    // Altitude High, expect a parameters like "35000ft" or "35000 ft"
+                    // Not implemented yet
+
+                    Log.d(TAG, "parseCommand: Altitude High");
+                    break;
+
+
+                case "DC":
+                    Log.d(TAG, "parseCommand: Draw Circle");
+
+                    // Draw Circle command - expect an integer argument
+                    radius = Integer.parseInt(rest) * 1852;
+                    pos = null;
+                    if (mCenter != null) {
+                        for (int deg = 0; deg < 360; deg++) {
+                            pos = SphericalUtil.computeOffset(mCenter, radius, deg);
+                            addPosToCoordList(pos);
+                        }
+                    }
+                    break;
+
                 case "V":
                     // Variable Assignment Command
                     // The pattern matches a variable name and the value argument from the rest of the line above
-                    String assignPattern="([\\w]+)\\s*=([\\s\\w\\d\\:\\.\\+\\-]*)";
+
+                    Log.d(TAG, "parseCommand: Variable assignment");
+
+                    String assignPattern = "([\\w]+)\\s*=([\\s\\w\\d\\:\\.\\+\\-]*)";
                     r = Pattern.compile(assignPattern);
                     m = r.matcher(rest);
 
-                    if (m.find()){
+                    if (m.find()) {
                         if (m.group(1).equals("D")) {
                             // Variable name D means this is a Direction assignment
-                            Log.d(TAG, "parseCommand: "+String.format("Direction command, sign: %s", m.group(2)));
+                            Log.d(TAG, String.format("Direction command, sign: %s", m.group(2)));
                             if (m.group(2).equals("+")) {
-                                mStep_direction=1;
+                                mStep_direction = 1;
                             } else {
-                                mStep_direction=-1;
+                                mStep_direction = -1;
                             }
 
                         } else {
                             // A position variable assignment, any variable name us supported although I have only seen X used
-                            Log.d(TAG, "parseCommand: "+String.format("Variable assignment. Variable: %s, rest: %s", m.group(1), m.group(2)));
+                            Log.d(TAG, String.format("Variable assignment: %s identified, remaining arguments: %s", m.group(1), m.group(2)));
 
-                            LatLng pos = parseCoordinateString(rest);
+                            pos = parseCoordinateString(rest);
                             if (pos != null) {
-                                Log.d(TAG, "parseCommand: setting mCenter to: "+pos);
+                                Log.d(TAG, "Setting mCenter to: " + pos);
                                 mCenter = pos;
+
                             } else {
                                 // If we cannot parse this as a position, we need to look into this later
-                                Log.e(TAG, "parseCommand: Might be some other kind of assignment...");
+                                Log.e(TAG, "parseCommand: Unsupported assignment...");
                             }
                         }
 
                     } else {
                         // We did not find anything useful in the arugument string after the name
-                        Log.d(TAG, "parseCommand: Variable parsing error");
+
+                        Log.d(TAG, "parseCommand: Variable argument parsing error");
                     }
 
                     break;
@@ -198,59 +315,51 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 case "DA":
                     // Draw Arc Command
                     // Pattern matches three comma separated integer aruments
+
+                    Log.d(TAG, "parseCommand: Draw Arc command");
+
                     String threeArgsPattern = "([\\d]+)\\s*\\,\\s*([\\d]+)\\s*\\,\\s*([\\d]+)";
                     r = Pattern.compile(threeArgsPattern);
                     m = r.matcher(rest);
 
                     if (m.find()) {
-                        int radius = Integer.parseInt(m.group(1))*1852;
-                        int fromDeg = Integer.parseInt(m.group(2));
-                        int toDeg = Integer.parseInt(m.group(3));
-
-                        Log.d(TAG, String.format("Radius: %d, from: %d to %d", radius, fromDeg, toDeg));
-
-                        if (mCenter!=null) {
-                            double x,y;
-                            LatLng newPos;
-                            int degrees = fromDeg;
-                            int step = mStep_direction*STEP_SIZE;
-                            do {
-                                newPos = SphericalUtil.computeOffset(mCenter, radius, degrees);
-                                addPosToCoordList(newPos);
-                                degrees+=step;
-                                if (Math.abs(((degrees+360) %360)-toDeg)<=STEP_SIZE)
-                                    break;
-                            } while (true);
-
-                        }
+                        radius = Integer.parseInt(m.group(1)) * 1852;
+                        fromDeg = Integer.parseInt(m.group(2));
+                        toDeg = Integer.parseInt(m.group(3));
+                        drawArcFromTo(radius, fromDeg, toDeg);
                     } else {
                         // We did not find the expected three integers in the argument string
-                        Log.d(TAG, "parseCommand: Draw arc parameters not recognized");
+                        Log.e(TAG, "parseCommand: Draw arc parameters not recognized");
                     }
                     break;
 
                 case "DP":
                     // Define Point Command
                     // Pattern matches a potential coordinate string
-                    String coordPattern="([\\d\\:\\. \\w]+)";
+
+                    Log.d(TAG, "parseCommand: Draw Point Commannd");
+
+                    String coordPattern = "([\\d\\:\\. \\w]+)";
                     r = Pattern.compile(coordPattern);
                     m = r.matcher(rest);
                     if (m.find()) {
-                        LatLng pos1 = parseCoordinateString(m.group(1));
-                        Log.d(TAG, "parseCommand: Got a coordinate : "+pos1);
-                        coordList.add(pos1);
+                        pos = parseCoordinateString(m.group(1));
+                        addPosToCoordList(pos);
+
+                        Log.d(TAG, "Got a coordinate : " + pos);
 
                     } else {
-                        Log.e(TAG, "parseCommand: Problem parsing DP argument" );
+                        Log.e(TAG, "parseCommand: Problem parsing DP argument");
                     }
                     break;
 
 
-
                 case "DB":
+                    // Draw Between Command
                     Log.d(TAG, "parseCommand: Draw between command");
+
                     // Pattern matches two possible coordinates separated by a comma
-                    String betweenPattern="([\\d\\:\\. \\w]+) *, *([\\d\\:\\. \\w]+)";
+                    String betweenPattern = "([\\d\\:\\. \\w]+) *, *([\\d\\:\\. \\w]+)";
                     r = Pattern.compile(betweenPattern);
                     m = r.matcher(rest);
 
@@ -258,29 +367,48 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                         LatLng pos1, pos2;
                         pos1 = parseCoordinateString(m.group(1));
                         pos2 = parseCoordinateString(m.group(2));
-                        Log.d(TAG, "parseCommand: Got two coordinates : "+pos1+" and "+pos2);
+                        Log.d(TAG, "parseCommand: Got two coordinates : " + pos1 + " and " + pos2);
+
+                        if (pos1 != null && pos2 != null) {
+                            fromDeg = (int) SphericalUtil.computeHeading(mCenter, pos1);
+                            toDeg = (int) SphericalUtil.computeHeading(mCenter, pos2);
+                            radius = (int) SphericalUtil.computeDistanceBetween(mCenter, pos1);
+                            drawArcFromTo(radius, fromDeg, toDeg);
+                        }
                     } else {
-                        Log.e(TAG, "parseCommand: Problem parsing draw between arguments" );
+                        Log.e(TAG, "parseCommand: Problem parsing draw between arguments");
                     }
                     break;
 
-                 default:
+                default:
                     Log.d(TAG, "parseCommand: not recognized");
                     break;
             }
 
 
-
-
         } else {
             Log.e(TAG, "parseCommand: Cannot parse command: " + cmd);
         }
+    }
 
+    private void drawArcFromTo(int radius, int fromDeg, int toDeg) {
+        if (mCenter != null) {
+            double x, y;
+            LatLng newPos;
+            int degrees = fromDeg;
+            int step = mStep_direction * STEP_SIZE;
+            do {
+                newPos = SphericalUtil.computeOffset(mCenter, radius, degrees);
+                addPosToCoordList(newPos);
+                degrees += step;
+                if (Math.abs(((degrees + 360) % 360) - toDeg) < STEP_SIZE)
+                    break;
+            } while (true);
 
-
+        }
     }
 
     public double compasToMathDegrees(double compass) {
-        return (double) (((90-compass)+360)%360);
+        return (double) (((90 - compass) + 360) % 360);
     }
 }
